c2af3edc7ac82a7df8d07ec98be834b1fefe2699
huangxinmin <h118080132020@outlook.com>
2021-05-06 20:09:44 +0800

    study_26

---
+++ src/study_26.java
@@ -0,0 +1,19 @@
+public class study_26 {
+    class Solution {
+        public int countPrimes(int n) {
+            int ans = 0;
+            for (int i = 2; i < n; ++i) {
+                ans += isPrime(i) ? 1 : 0;
+            }
+            return ans;
+        }
+        public boolean isPrime(int x) {
+            for (int i = 2; i * i <= x; ++i) {
+                if (x % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
