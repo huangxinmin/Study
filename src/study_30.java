1837230882c7484fed561372e0a86943f3da1f75
huangxinmin <h118080132020@outlook.com>
2021-05-10 17:59:27 +0800

    study_30

---
+++ src/study_30.java
@@ -0,0 +1,37 @@
+public class study_30  {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public class Solution {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            if (headA == null || headB == null) {
+                return null;
+            }
+
+            ListNode head1 = headA;
+            ListNode head2 = headB;
+
+            while (head1 != head2) {
+                if (head1 != null) {
+                    head1 = head1.next;
+                } else {
+                    head1 = headB;
+                }
+
+                if (head2 != null) {
+                    head2 = head2.next;
+                } else {
+                    head2 = headA;
+                }
+            }
+            return head2;
+        }
+    }
+}
+
