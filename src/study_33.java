082ecbd547b72bcd9a9d33220486794a8a8f2cdc
huangxinmin <h118080132020@outlook.com>
2021-05-13 23:58:21 +0800

    study_33

---
+++ src/study_33.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class study_33 {
+    class Solution {
+        public int missingNumber(int[] nums) {
+            Arrays.sort(nums);
+
+            if (nums[nums.length-1] != nums.length) {
+                return nums.length;
+            }
+            else if (nums[0] != 0) {
+                return 0;
+            }
+            for (int i = 1; i < nums.length; i++) {
+                int expectedNum = nums[i-1] + 1;
+                if (nums[i] != expectedNum) {
+                    return expectedNum;
+                }
+            }
+            return 123;
+        }
+    }
+}
