2d08c9bd354a9b4282b3be665b6d7dbda9d91c90
huangxinmin <h118080132020@outlook.com>
2021-05-17 20:29:44 +0800

    study_36

---
+++ src/study_36.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class study_36 {
+    class Solution {
+        public int[] intersection(int[] nums1, int[] nums2) {
+            Arrays.sort(nums1);
+            Arrays.sort(nums2);
+
+            int i = 0, j = 0;
+            HashSet<Integer> set = new HashSet<>();
+            while (i < nums1.length && j < nums2.length) {
+                if (nums1[i] == nums2[j]) {
+                    set.add(nums1[i]);
+                    i++;
+                    j++;
+                } else if (nums1[i] < nums2[j]) {
+                    i++;
+                } else {
+                    j++;
+                }
+            }
+            int[] res = new int[set.size()];
+            int index = 0;
+            for (int num : set) {
+                res[index++] = num;
+            }
+            return res;
+        }
+    }
+}
