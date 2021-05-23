f18b8f499500f4502edd3638a9d478a6df1a317e
huangxinmin <h118080132020@outlook.com>
2021-05-23 17:44:54 +0800

    study_41

---
+++ src/study_41.java
@@ -0,0 +1,25 @@
+public class study_41 {
+
+      public class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode(int x) { val = x; }
+      }
+
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            TreeNode ancestor = root;
+            while (true) {
+                if (p.val < ancestor.val && q.val < ancestor.val) {
+                    ancestor = ancestor.left;
+                } else if (p.val > ancestor.val && q.val > ancestor.val) {
+                    ancestor = ancestor.right;
+                } else {
+                    break;
+                }
+            }
+            return ancestor;
+        }
+    }
+}
