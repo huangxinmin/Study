f5c0543ecb4f9147fc39e6f6ac8ac678e348ca77
huangxinmin <h118080132020@outlook.com>
2021-06-05 17:10:04 +0800

    study_53

--- src/study_53.java
+++ src/study_53.java
@@ -1,22 +1,27 @@
 public class study_53 {
     class Solution {
-        public boolean checkPossibility(int[] nums) {
-            int count = 0;
-            for (int i = 0; i < nums.length - 1; i++) {
-                //数组中超过一个地方呈递减数列则数组不成立
-                if (nums[i] > nums[i + 1]) {
-                    count++;
-                    if (count > 1) {
-                        return false;
-                    }
-                    //保证数组成立需要确保i-1<i<=i+1
-                    if (i > 0 && nums[i + 1] < nums[i - 1]) {
-                        nums[i + 1] = nums[i];
-                    }
-                }
+        public boolean isUgly(int n) {
+            //0和负数一定不能被2，3，5分解
+            if (n <= 0) {
+                return false;
             }
-            return true;
+            //循环到不能被2，3，5整除为止
+            while(n >= 2) {
+                if(n % 2 == 0) {
+                    n /= 2;
+                    continue;
+                }
+                if(n % 3 == 0) {
+                    n /= 3;
+                    continue;
+                }
+                if(n % 5 == 0) {
+                    n /= 5;
+                    continue;
+                }
+                break;
+            }
+            return n == 1;
         }
     }
 }
-
