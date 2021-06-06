f1a38548d28c96d75c9e0d1925ca04ef72060756
huangxinmin <h118080132020@outlook.com>
2021-06-06 18:47:08 +0800

    study_54

--- src/study_54.java
+++ src/study_54.java
@@ -1,18 +1,21 @@
 public class study_54 {
     class Solution {
-        public boolean checkPossibility(int[] nums) {
-            int count = 0;
-            for (int i = 0; i < nums.length - 1; i++) {
-                //数组中超过一个地方呈递减数列则数组不成立
-                if (nums[i] > nums[i + 1]) {
-                    count++;
-                    if (count > 1) {
-                        return false;
-                    }
-                    //保证数组成立需要确保i-1<i<=i+1
-                    if (i > 0 && nums[i + 1] < nums[i - 1]) {
-                        nums[i + 1] = nums[i];
-                    }
+        public boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+            //将字符串存在哈希表中
+            int [] character = new int[26];
+            //遍历字符串s并统计字母出现的频次
+            for (int i = 0; i < s.length(); i++) {
+                character[s.charAt(i) - 'a']++;
+            }
+            //遍历字符串t并统计字母出现的频次，并减去character中出现的频次，
+            //如果出现character[i]<0，则说明字符串t包含s字符串中不存在的字符
+            for (int i = 0; i <t.length(); i++) {
+                character[t.charAt(i) - 'a']--;
+                if (character[t.charAt(i) - 'a'] < 0) {
+                    return false;
                 }
             }
             return true;
